6ba1e825776e587508f48c0d6de1ac70de4dae79
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-11 20:39:57 +0000

    added a js file for implementing textarea remaining letters allowed counter function

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').on("input", function(e) {
+    const length = $('.new-tweet textarea').val().length;
+    const counter = $('.new-tweet .counter');
+    //const value = Number(counter);
+    let value = 140;
+    if (e.keyCode === 8) {
+      if (length < 140) {
+        value = 140 - length;
+      }
+      else {
+        value++;
+      }
+    }
+    else {
+      value = 140 - length;
+    }
+
+    counter.toggleClass("turn-red", (value < 0));
+
+    counter.text(value);
+  });
+});
