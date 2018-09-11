a91bf9d427a612d7e611e9e9cb77ac9d93ba52bb
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-11 21:20:35 +0000

    fixed createTweet function to work properly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,54 +10,55 @@
 // Fake data taken from tweets.json
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    user: {
+      name: 'Newton',
+      avatars: {
+        small: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
+        regular: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
+        large: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png',
       },
-      "handle": "@SirIsaac"
+      handle: '@SirIsaac',
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants',
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+    user: {
+      name: 'Descartes',
+      avatars: {
+        small: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
+        regular: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
+        large: 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png',
       },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+      handle: '@rd',
     },
-    "created_at": 1461113959088
+    content: {
+      text: 'Je pense , donc je suis',
+    },
+    created_at: 1461113959088,
   },
   {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+    user: {
+      name: 'Johann von Goethe',
+      avatars: {
+        small: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png',
+        regular: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png',
+        large: 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png',
       },
-      "handle": "@johann49"
+      handle: '@johann49',
     },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    content: {
+      text: 'Es ist nichts schrecklicher als eine tätige Unwissenheit.',
     },
-    "created_at": 1461113796368
-  }
+    created_at: 1461113796368,
+  },
 ];
 
 function renderTweet(tweets) {
   for (tweet of tweets) {
-    $('#tweet-container').append(createTweetElement(tweet));
+    $('.tweet-container').append(createTweetElement(tweet));
   }
 }
 
@@ -72,23 +73,23 @@ function createTweetElement(tweet) {
   const tweetTime = tweet.created_at;
 
   // overarching elements for the tweet container
-  const $header = $('header').addClass('tweet-header');
-  const $body = $('p').addClass('tweet-content');
-  const $footer = $('footer').addClass('tweet-footer');
+  const $header = $('<header>').addClass('tweet-header');
+  const $body = $('<p>').addClass('tweet-content');
+  const $footer = $('<footer>').addClass('tweet-footer');
 
-  //
-  const $headerIcon = $('img').addClass('tweet-header__profile-pic');
-  const $headerUsername = $('h2').addClass('tweet-header__user-name');
-  const $headerHandleName = $('h5').addClass('tweet-header__handle-name');
-  const $content = $('p').addClass('tweet-content');
-  const $footerTimestamp = $('div').addClass('tweet-footer__timestamp');
+  // individual elements for each variable that stores the hard data.
+  const $headerIcon = $('<img>').addClass('tweet-header__profile-pic');
+  const $headerUsername = $('<h2>').addClass('tweet-header__user-name');
+  const $headerHandleName = $('<h5>').addClass('tweet-header__handle-name');
+  const $content = $('<p>').addClass('tweet-content');
+  const $footerTimestamp = $('<div>').addClass('tweet-footer__timestamp');
 
-  //appends the information into their respective elements
-  $headerIcon.append(iconImg);
-  $headerUsername.append(username);
-  $headerHandleName.append(handleName);
-  $content.append(tweetContent);
-  $footerTimestamp.append(tweetTime);
+  // appends the information into their respective elements
+  $headerIcon.attr('src', iconImg);
+  $headerUsername.text(username);
+  $headerHandleName.text(handleName);
+  $content.text(tweetContent);
+  $footerTimestamp.text(tweetTime);
 
   //appends the above elements to their parents
   $header.append($headerIcon);
@@ -109,4 +110,7 @@ function createTweetElement(tweet) {
 // console.log($tweet);
 // $('#tweet-container').append($tweet);
 
-renderTweet(data);
+$(() => {
+  renderTweet(data);
+});
+
