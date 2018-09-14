144dbc31670aee8dbc00c1fcc0901d568e9c2772
Patrick Chu <mingweipatrickc@gmail.com>
2018-09-14 17:38:57 +0000

    fixed indentation as well as line length to improve readability

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,8 +7,8 @@ const simulateDelay = require('./util/simulate-delay');
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-    //Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
+    // Saves a tweet to `db`
+    saveTweet: (newTweet, callback) => {
       db.collection('tweets').insert(newTweet);
       callback(null, true);
     },
@@ -16,22 +16,22 @@ module.exports = function makeDataHelpers(db) {
     // Get all tweets in `db`, sorted by newest first
     getTweets: (callback) => {
       db.collection('tweets').find().sort({ created_at: 1 })
-      .toArray((err, result) => {
+        .toArray((err, result) => {
           // Lazy error handling:
           if (err) throw err;
 
           callback(null, result);
-      });
+        });
     },
 
-    // save likes
+    // updates like_count for a certain tweetin database by value,
+    // which is passed in through tweets.js
     toggleLikes: (tweet, callback) => {
-      db.collection('tweets').findOneAndUpdate({ _id: ObjectId(tweet.body.id) }, { $inc: { like_count: Number(tweet.body.value) } },{returnOriginal: false, }, (err, result) => {
-        //console.log(result.value.like_count);
-        callback(null, result.value);
-      });
-      // db.collection('tweets').update({'_id': tweet.body.id}, {like_count: })
-
-    }
-  }
-}
+      db.collection('tweets').findOneAndUpdate({ _id: ObjectId(tweet.body.id) },
+        { $inc: { like_count: Number(tweet.body.value) } },
+        { returnOriginal: false }, (err, result) => {
+          callback(null, result.value);
+        });
+    },
+  };
+};
